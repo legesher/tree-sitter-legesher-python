efe16fc63491ed4754f2ee8b4e42e440dd8634d4
Madi Edgar <withmadico@gmail.com>
2019-09-09 18:22:31 -0400

    :bug: fix tree_sitter_legesher_python spelling

--- src/binding.cc
+++ src/binding.cc
@@ -4,7 +4,7 @@
 
 using namespace v8;
 
-extern "C" TSLanguage * tree_sitter_legesher_python();
+extern "C" TSLanguage * tree_sitter_python_legesher();
 
 namespace {
 
@@ -17,12 +17,12 @@ void Init(Local<Object> exports, Local<Object> module) {
 
   Local<Function> constructor = Nan::GetFunction(tpl).ToLocalChecked();
   Local<Object> instance = constructor->NewInstance(Nan::GetCurrentContext()).ToLocalChecked();
-  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_legesher_python());
+  Nan::SetInternalFieldPointer(instance, 0, tree_sitter_python_legesher());
 
   Nan::Set(instance, Nan::New("name").ToLocalChecked(), Nan::New("python").ToLocalChecked());
   Nan::Set(module, Nan::New("exports").ToLocalChecked(), instance);
 }
 
-NODE_MODULE(tree_sitter_legesher_python_binding, Init)
+NODE_MODULE(tree_sitter_python_legesher_binding, Init)
 
 }  // namespace
