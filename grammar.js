77ecce70885b83ce9e5a1a04762128e3fe3a85c8
alxnull <alxnull@e.mail.de>
2020-10-04 00:33:41 +0200

    Sync: Support splatting in tuples Commit: 132f9a5745187dc5cb3f2c993d12319f6947b3c

--- grammar.js
+++ grammar.js
@@ -6,6 +6,7 @@ const PREC = {
   conditional: -1,
 
   parenthesized_expression: 1,
+  parenthesized_list_splat: 1,
   not: 1,
   compare: 2,
   or: 10,
@@ -350,15 +351,14 @@ module.exports = grammar({
         field("body", $._suite)
       ),
 
-    parenthesized_list_splat: $ =>
-      seq(
+      parenthesized_list_splat: $ => prec(PREC.parenthesized_list_splat, seq(
         "(",
         choice(
           alias($.parenthesized_list_splat, $.parenthesized_expression),
           $.list_splat
         ),
         ")"
-      ),
+      )),
 
     argument_list: $ =>
       seq(
@@ -676,8 +676,7 @@ module.exports = grammar({
     list: $ =>
       seq(
         "[",
-        optional(commaSep1(choice($._expression, $.list_splat))),
-        optional(","),
+        optional($._collection_elements),
         "]"
       ),
 
@@ -718,11 +717,17 @@ module.exports = grammar({
         seq("(", choice($._expression, $.yield), ")")
       ),
 
+    _collection_elements: $ => seq(
+      commaSep1(choice(
+        $._expression, $.yield, $.list_splat, $.parenthesized_list_splat
+      )),
+      optional(',')
+    ),
+
     tuple: $ =>
       seq(
         "(",
-        optional(commaSep1(choice($._expression, $.yield))),
-        optional(","),
+        optional($._collection_elements),
         ")"
       ),
 
